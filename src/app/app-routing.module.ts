747e8bc51511dd77440f26a789d144d8a41d6b35
Naveed <naveedfarouq@hotmail.co.uk>
2019-03-30 16:18:12 +0000

    adding another component for lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from '../app/home/home.component';
+import { HelloComponent } from './hello/hello/hello.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'}
+  {path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
+  {path: 'hello', loadChildren: './hello/hello.module#HelloModule'},
+  //{path: 'hello', component: HelloComponent}
+
 ];
 
 @NgModule({
